ac98e0eb11c6d226e43b12adead8cd1789404398
Alex Milenkovic <jk1143298@gmail.com>
2023-10-12 00:23:40 +0200

    abstract classes

--- is_iter_benchmark.py
+++ is_iter_benchmark.py
@@ -1,5 +1,7 @@
 from time import time
 
+from collections.abc import Iterable
+
 
 def is_iter(v):
     try:
@@ -7,27 +9,34 @@ def is_iter(v):
     except TypeError: return False
     return True
 
+def isiter(v):
+    return isinstance(v, Iterable)
+
 
 def _is_iter(v):
     try: iter(v)
     except TypeError: return False
     return True
 
-def benchmark():
-    l = [1] * 1000000
+def __is_iter(v):
+    try: v.__iter__
+    except AttributeError: return False
+    return True
 
+def benchmark(*args):
     SIZE = 100
-    avg = 0
 
-    for i in range(0, SIZE):
-        start = time()
-        for i in range(0, 10000):
-            is_iter(l)
-        avg += time() - start
+    for arg in args:
+        avg = 0
+        for i in range(0, SIZE):
+            start = time()
+            for i in range(0, 10000):
+                is_iter(arg)
+            avg += time() - start
 
-    print('avg: ', avg / SIZE )
+        print(str(type(arg)) + ' avg: ', avg / SIZE )
 
 if __name__ == '__main__':
 
-    benchmark()
+    benchmark([1] * 100000, 1, 'aaa', (1, 3) )
 
